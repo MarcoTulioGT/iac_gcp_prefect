78fbe30bf1676e843bc0c71bee9df19009f4007f
MarcoTulioGT <devopsgt@gmail.com>
2025-07-10 19:06:23 -0600

    gcp inicial

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  description = "The GCP project ID where resources will be created"
+  type        = string
+ }
+
+variable "region" {
+  type       = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type       = string
+  default = "us-central1-c"
+}
+
