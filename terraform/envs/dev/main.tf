02d976f302b5fc5c0a3aa17a3c221c4c1dece9ef
MarcoTulioGT <devopsgt@gmail.com>
2025-07-26 17:56:25 -0600

    format debug

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -5,32 +5,32 @@ provider "google" {
 }
 
 module "network" {
-  source              = "../../modules/network"
-  instance_name       = "postgresql-instance-dev"
-  machine_type        = "f1-micro"
-  environment         = "dev"
-  project             = var.project
-  region              = var.region
-  zone                = var.zone
-  network_name        = "default"
-  policy_name         = "allow-prefect-ports"
-  allow_protocol      = "tcp"
-  allow_ports         = ["22", "8080", "3000", "4200", "5432"]
-  source_ranges       = ["0.0.0.0/0"]
-  description         = "Allow SSH, HTTP app, and other custom ports"
+  source         = "../../modules/network"
+  instance_name  = "postgresql-instance-dev"
+  machine_type   = "f1-micro"
+  environment    = "dev"
+  project        = var.project
+  region         = var.region
+  zone           = var.zone
+  network_name   = "default"
+  policy_name    = "allow-prefect-ports"
+  allow_protocol = "tcp"
+  allow_ports    = ["22", "8080", "3000", "4200", "5432"]
+  source_ranges  = ["0.0.0.0/0"]
+  description    = "Allow SSH, HTTP app, and other custom ports"
 }
 module "postgresql" {
-  source              = "../../modules/postgresql"
-  instance_name       = "postgresql-instance-dev"
-  machine_type        = "f1-micro"
-  environment         = "dev"
-  project             = var.project
-  region              = var.region
-  zone                = var.zone
-  description         = "postgresql-instance-for-development-environment"
-  provisioning_model  = "SPOT"
-  preemptible         = true
-  automatic_restart   = false
-  boot_image          = "debian-cloud/debian-12"
-  network_name        =  "default"
+  source             = "../../modules/postgresql"
+  instance_name      = "postgresql-instance-dev"
+  machine_type       = "f1-micro"
+  environment        = "dev"
+  project            = var.project
+  region             = var.region
+  zone               = var.zone
+  description        = "postgresql-instance-for-development-environment"
+  provisioning_model = "SPOT"
+  preemptible        = true
+  automatic_restart  = false
+  boot_image         = "debian-cloud/debian-12"
+  network_name       = "default"
 }
