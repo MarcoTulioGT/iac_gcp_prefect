b0ddba4b54cfd13cb8fc82de574ed70db5247062
MarcoTulioGT <devopsgt@gmail.com>
2025-07-26 17:03:52 -0600

    quite mayusculas en desctiption

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -12,5 +12,5 @@ module "postgresql" {
   project       = var.project
   region        = var.region
   zone          = var.zone
-  description   = "PostgreSQL instance for development environment"
+  description   = "postgresql instance for development environment"
 }
