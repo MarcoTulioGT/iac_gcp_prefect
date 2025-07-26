76438e2743d925f0424b8fa82e38150a2d38b4cb
MarcoTulioGT <devopsgt@gmail.com>
2025-07-26 17:23:12 -0600

    cambie el output

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,3 +1,14 @@
-output "instance_ip" {
-  value = google_compute_instance.postgresql_instance.network_interface[0].access_config[0].nat_ip
+output "firewall_name" {
+  description = "Nombre de la regla de firewall"
+  value       = google_compute_firewall.allow_prefect_ports.name
+}
+
+output "allowed_ports" {
+  description = "Puertos permitidos por la regla"
+  value       = [for rule in google_compute_firewall.allow_prefect_ports.allow : rule.ports]
+}
+
+output "network_used" {
+  description = "Red asociada a la regla de firewall"
+  value       = google_compute_firewall.allow_prefect_ports.network
 }
