94bd01d0b471c93a8d7d64cf812d9bd2c164dae2
MarcoTulioGT <devopsgt@gmail.com>
2025-07-25 22:24:50 -0600

    ok 1

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,9 +1,15 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
 module "postgresql" {
   source        = "../../modules/postgresql"
   instance_name = "postgresql-instance-dev"
   machine_type  = "f1-micro"
   environment   = "dev"
-  project       = var.project  
+  project       = var.project
   region        = var.region
   zone          = var.zone
 }
