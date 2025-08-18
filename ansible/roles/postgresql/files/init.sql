fbee1eded278167b4a7de7fb685a826fd389ab74
MarcoTulioGT <devopsgt@gmail.com>
2025-08-18 17:33:14 -0600

    add library psycopg2

--- ansible/roles/postgresql/files/init.sql
+++ ansible/roles/postgresql/files/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE empleados (
     fecha_contratacion DATE
 );
 insert into empleados(nombre, salario, correo_electronico, dpi, fecha_contratacion) values ('Marco',3500,'Marco@gmail.com','3454655667877','2011-04-11');
-insert into empleados(nombre, salario, correo_electronico, dpi, fecha_contratacion) values ('Juan',4000,'Juan@gmail.com','876754634567987','2019-02-11');
+insert into empleados(nombre, salario, correo_electronico, dpi, fecha_contratacion) values ('Juan',4000,'Juan@gmail.com','8767546345679','2019-02-11');
 insert into empleados(nombre, salario, correo_electronico, dpi, fecha_contratacion) values ('Manuel',2000,'Manuel@gmail.com','5434234345654','2009-02-16');
 insert into empleados(nombre, salario, correo_electronico, dpi, fecha_contratacion) values ('Pedro',1500,'Pedro@gmail.com','0987677846576','2005-02-14');
 insert into empleados(nombre, salario, correo_electronico, dpi, fecha_contratacion) values ('Carlos',5000,'Carlos@gmail.com','3654376546876','2024-02-19');
---
+++ prefect/blocks/block_db.py
@@ -0,0 +1,3 @@
+from prefect.blocks.system import String
+
+string_block = String.load("db-ip")
