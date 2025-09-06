963e69044bed376c4bf2862eb21de16aef0e42fa
MarcoTulioGT <devopsgt@gmail.com>
2025-09-06 09:04:05 -0600

    add changes

--- prefect/flows/database.py
+++ prefect/flows/database.py
@@ -1,3 +1,4 @@
+# flows/database.py
 from prefect import flow
 
 @flow
@@ -5,10 +6,4 @@ def my_flow():
     print("Hola Prefect local")
 
 if __name__ == "__main__":
-    my_flow.deploy(
-        name="local-deployment",
-        work_pool_name="default-local-pool",
-        tags=["dev"],
-        push=True
-
-    )
+    my_flow()
